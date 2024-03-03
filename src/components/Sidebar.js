59d71611b8b33c5c422a1110fd0b49ef3874528a
Kipleting Edwin <kipletingedwin4@gmail.com>
2024-03-03 22:16:58 +0300

    add map method

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,9 +1,27 @@
 import React from 'react'
 import '../App.css';
+import { SidebarData } from './SidebarData';
 
 function Sidebar() {
   return (
-    <div className='Sidebar'>Sidebar</div>
+    <div className='Sidebar'>
+      <ul> 
+      {
+        SidebarData.map((val, id)=>{
+          return(
+            <li key={id} onClick={window.location.pathname = val.link}>
+              <div>
+                {val.icon}
+              </div>
+              <div>
+                {val.title}
+              </div>
+            </li>
+          )
+        })
+      }
+      </ul>
+    </div>
   )
 }
 
