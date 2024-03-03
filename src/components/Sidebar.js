49231f41574dcfe843b8f130fe339af5c05816be
Kipleting Edwin <kipletingedwin4@gmail.com>
2024-03-03 23:22:21 +0300

    finalise on navbar features

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,28 +1,29 @@
-import React from 'react'
-import '../App.css';
-import { SidebarData } from './SidebarData';
+import React from "react";
+import "../App.css";
+import { SidebarData } from "./SidebarData";
 
 function Sidebar() {
   return (
-    <div className='Sidebar'>
-      <ul className='SidebarList'> 
-      {
-        SidebarData.map((val, id)=>{
-          return(
-            <li key={id}  className='row'>
-              <div id='icon'>
-                {val.icon}
-              </div>
-              <div id='title'>
-                {val.title}
-              </div>
+    <div className="Sidebar">
+      <ul className="SidebarList">
+        {SidebarData.map((val, id) => {
+          return (
+            <li
+              key={id}
+              className="row"
+              id={window.location.pathname === val.link ? "active" : ""}
+              onClick={() => {
+                window.location.pathname = val.link;
+              }}
+            >
+              <div id="icon">{val.icon}</div>
+              <div id="title">{val.title}</div>
             </li>
-          )
-        })
-      }
+          );
+        })}
       </ul>
     </div>
-  )
+  );
 }
 
-export default Sidebar
+export default Sidebar;
